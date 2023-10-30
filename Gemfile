8a4ee47d38eb55d8076e7eee3b9cd5d8c6578fdb
joaquin <joaquinarielcasal@gmail.com>
2023-10-30 01:30:56 -0300

    Fix gemfile

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[mri mingw x64_mingw]
   gem "rspec-rails", "~> 6.0.0"
+  gem 'rubocop-rails', require: false
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,6 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
