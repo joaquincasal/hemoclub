d9c1af68406b48514db0adc45104a976d66e1728
Joaquin <joaquinarielcasal@gmail.com>
2023-10-30 01:33:56 -0300

    Rails upgrade (#21)
    * Upgrade
    
    * rspec migration
    
    * Fix gemfile

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module Myapp2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.test_framework = :rspec
   end
 end
