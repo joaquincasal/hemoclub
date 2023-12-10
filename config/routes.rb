c20276555582da96a46f76af68ae87e0aec39511
joaquin <joaquinarielcasal@gmail.com>
2023-12-09 21:16:38 -0300

    Traducir rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  resources :clinicas, param: :codigo, only: [:index, :new, :edit, :create, :update]
-  devise_for :usuarios
-  resources :donantes do
-    collection do
-      get "importar", to: "donantes#import"
-      post "importar", to: "donantes#do_import"
+  scope(path_names: { new: 'nueva', edit: 'editar' }) do
+    resources :clinicas, param: :codigo, only: [:index, :new, :edit, :create, :update]
+    devise_for :usuarios, path_names: {
+      sign_in: 'iniciar_sesion', sign_out: 'cerrar_sesion',
+      password: 'contraseña', confirmation: 'confirmacion',
+      invitation: 'invitaciones'
+    }
+
+    resources :donantes do
+      collection do
+        get "importar", to: "donantes#import"
+        post "importar", to: "donantes#do_import"
+      end
     end
+    # Defines the root path route ("/")
+    root "donantes#index"
+    get "up" => "rails/health#show", as: :rails_health_check
   end
-  # Defines the root path route ("/")
-  root "donantes#index"
-  get "up" => "rails/health#show", as: :rails_health_check
 end
