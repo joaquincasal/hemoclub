964dca97dca3dea684bf79f3db1ef4c4ce8b5362
joaquin <joaquinarielcasal@gmail.com>
2024-05-21 00:05:50 -0300

    Upgrades

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-Rails.application.default_url_options = { host: ENV.fetch("APP_HOST") }
+Rails.application.default_url_options = { host: ENV.fetch("APP_HOST", "") }
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
