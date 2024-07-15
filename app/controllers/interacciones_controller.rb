a94997add346cb809be8e532c00bfccae3578bbc
joaquin <joaquinarielcasal@gmail.com>
2024-07-15 01:19:16 -0300

    Prod config

--- app/controllers/interacciones_controller.rb
+++ app/controllers/interacciones_controller.rb
@@ -2,7 +2,12 @@ class InteraccionesController < ApplicationController
   skip_before_action :verify_authenticity_token
   skip_before_action :authenticate_usuario!
 
+  # rubocop:disable Metrics/AbcSize,Lint/UnreachableCode
   def update
+    return head :unauthorized if ENV["WEBHOOK_KEY"] != params[:key]
+
+    return head :ok
+
     evento = JSON.parse(request.raw_post)
     id_mensaje = evento["messageId"]
     if evento["eventType"] == "Microsoft.Communication.EmailDeliveryReportReceived"
@@ -13,4 +18,5 @@ class InteraccionesController < ApplicationController
       Interaccion.find_by(id_mensaje: id_mensaje).marcar_leido
     end
   end
+  # rubocop:enable Metrics/AbcSize,Lint/UnreachableCode
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,6 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: ENV["MAILER_HOST"] }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV["MAILER_USERNAME"],
